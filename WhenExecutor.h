9b658c78c79824345b44e0e90a3e5a1ccb42e5ee
Krishneel Sahdeo <krishneel_sahdeo@yahoo.com>
2018-02-27 19:40:13 -0800

    shot in the dark
    praying

--- WhenExecutor.h
+++ WhenExecutor.h
@@ -3,13 +3,45 @@
  *
  *  Created on: Feb 27, 2018
  *      Author: shervin
+
+ * <h1>WhenExecutor</h1>
+ *
+ * <p>Execute an WHEN statement.</p>
+ *
+ * <p>Copyright (c) 2017 by Ronald Mak</p>
+ * <p>For instructional purposes only.  No warranties.</p>
  */
+#ifndef WHENEXECUTOR_H_
+#define WHENEXECUTOR_H_
 
-#ifndef WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_
-#define WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_
+#include "StatementExecutor.h"
+#include "../../../Object.h"
+#include "../../../intermediate/ICodeNode.h"
 
+namespace wci { namespace backend { namespace interpreter { namespace executors {
 
+using namespace std;
+using namespace wci;
+using namespace wci::backend::interpreter;
+using namespace wci::intermediate;
 
+class WhenExecutor : public StatementExecutor
+{
+public:
+    /**
+     * Constructor.
+     * @param the parent executor.
+     */
+    WhenExecutor(Executor *parent);
 
+    /**
+     * Execute a compound statement.
+     * @param node the root node of the compound statement.
+     * @return nullptr.
+     */
+    Object execute(ICodeNode *node);
+};
 
-#endif /* WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_ */
+}}}}  // namespace wci::backend::interpreter::executors
+
+#endif /* WHENEXECUTOR_H_ */
