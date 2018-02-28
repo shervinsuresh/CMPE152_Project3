e501e69cc0dcf9e23ce19d8270f1d29c001588b8
shervinsuresh <36718769+shervinsuresh@users.noreply.github.com>
2018-02-27 19:27:27 -0800

    Update PascalToken.cpp
    ADDED THE STUFF FOR OTHERWISE

--- PascalToken.cpp
+++ PascalToken.cpp
@@ -31,7 +31,7 @@ void PascalToken::initialize()
         "ELSE", "END", "FILE", "FOR", "FUNCTION", "GOTO", "IF", "IN",
         "LABEL", "MOD", "NIL", "NOT", "OF", "OR", "PACKED", "PROCEDURE",
         "PROGRAM", "RECORD", "REPEAT", "SET", "THEN", "TO", "TYPE",
-        "UNTIL", "VAR", "WHILE", "WITH","WHEN"
+        "UNTIL", "VAR", "WHILE", "WITH","WHEN", "OTHERWISE",
     };
 
     vector<PascalTokenType> rw_keys =
@@ -75,7 +75,9 @@ void PascalToken::initialize()
         PascalTokenType::VAR,
         PascalTokenType::WHILE,
         PascalTokenType::WITH,
-        PascalTokenType::WHEN
+        PascalTokenType::WHEN,
+        PascalTokenType::OTHERWISE
+        
 
     };
 
